360012cc2b1ba5d9b826f8b56b47f3f32950d455
bianyuanop <m13320507359@163.com>
2021-06-19 20:05:01 +0800

    change UI lib

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { ElCard } from 'element-plus'
+
+export default defineComponent({
+  setup() {
+    
+  },
+  components: {
+    ElCard
+  }
+  
+})
+</script>
+
+<template>
+<el-card >
+  <span>欢迎</span> 
+  <span>Welcome</span> 
+</el-card>
+</template>
+
+<style scoped>
+.home {
+  top: 30%;
+}
+.home-central {
+  text-align: center;
+}
+</style>
