2f0d1024edc495a7d5fc824cca284369df05f001
bianyuanop <m13320507359@163.com>
2021-06-20 22:32:59 +0800

    home page added

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,19 @@ export default {
 <style>
 html, body {
   height: 100%;
+  overflow: hidden;
 }
 #app {
   height: 100%;
   width: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+@font-face {
+  font-family: 'Chalk';
+  src: url('~@/assets/fonts/KGBlankSpaceSketch.ttf');
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,68 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { ElCard } from 'element-plus'
 
 export default defineComponent({
   setup() {
     
   },
   components: {
-    ElCard
   }
   
 })
 </script>
 
 <template>
-<el-card >
-  <span>欢迎</span> 
-  <span>Welcome</span> 
-</el-card>
+<div class="home">
+  <span class="title">kmfls</span>
+  <span class="desc">where old memories belong.</span>
+  <div class="btn">Click to start ►</div>
+</div>
+<img src="@/assets/img/home-background.jpg" alt="home-background" class="bg">
 </template>
 
 <style scoped>
+.btn {
+  border-radius: 18px;
+  border-style: solid;
+  border-width: 1px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  padding-left: 5%;
+  padding-right: 5%;
+  margin-top: 10%;
+}
+
+.btn:hover {
+  cursor: pointer;
+  background: whitesmoke;
+}
+
 .home {
-  top: 30%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  padding: 5%;
+
+  background: rgba(130, 133, 128, 0.658);
 }
 .home-central {
   text-align: center;
+}
+.title {
+  font-family: 'Chalk';
+  font-size: 100px;
+}
+.desc {
+  margin-top: -15px;
+  font-size: 15px;
+}
+img.bg {
+  position: absolute;
+  z-index: -1;
 }
 </style>
